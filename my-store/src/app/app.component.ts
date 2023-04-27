436753ec26bc74a3e9891d93c673163514c26136
Doplax <doplax@gmail.com>
2023-04-27 19:49:58 +0200

    escuchando eventos en botones y cajas de texto

--- my-store/src/app/app.component.ts
+++ my-store/src/app/app.component.ts
@@ -22,4 +22,14 @@ export class AppComponent {
   public increaseAge() {
     this.person.age = this.person.age + 1;
   }
+
+  onScroll(event:Event) {
+    const element = event.target as HTMLElement;
+    console.log(element.scrollTop);
+  }
+
+  changeName(event:Event){
+    const nameValue = event.target as HTMLInputElement;
+    this.person.name = nameValue.value
+  }
 }
