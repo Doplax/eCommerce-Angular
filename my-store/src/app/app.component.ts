b139ec4fa293617cdb1609b54e1f08012880d161
Doplax <doplax@gmail.com>
2023-04-27 19:05:46 +0200

    Jugando con angular

---
+++ my-store/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  public name = 'Pol';
+  public age = 26;
+  img = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/2048px-Angular_full_color_logo.svg.png'
+}
