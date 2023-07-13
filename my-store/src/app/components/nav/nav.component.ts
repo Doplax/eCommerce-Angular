d2eb8b4148cfd19adce2cdd8edb8b51ee10d4079
Doplax <doplax@gmail.com>
2023-07-13 13:19:27 +0200

    feature: 	- Side menu implementation

--- my-store/src/app/components/nav/nav.component.ts
+++ my-store/src/app/components/nav/nav.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.scss']
 })
-export class NavComponent {
+export class NavComponent implements OnInit {
+  activeMenu = false;
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  toggleMenu(): void {
+    this.activeMenu = !this.activeMenu;
+  }
 
 }
