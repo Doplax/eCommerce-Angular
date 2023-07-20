14d1d763729af39c1220aff386b673f41b705cd5
Doplax <doplax@gmail.com>
2023-07-20 09:52:45 +0200

    feat: 	- Implementación de retry/rxjs para getAllProducts()

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient , HttpParams } from '@angular/common/http';
+import { retry } from 'rxjs/operators';
+
 import { Product,CreateProductDTO, UpdateProductDTO }from '../interfaces/product.interface';
 
 
@@ -21,7 +23,12 @@ export class ProductsService {
       params.set('limit', limit);
       params.set('offset', offset);
     }
-    return this.Http.get<Product[]>(this.apiUrl,{params});
+    return this.Http.get<Product[]>(this.apiUrl,{params})
+      .pipe(
+        retry(3)
+      )
+    ;
+
   }
 
   getProduct(id:number) {
