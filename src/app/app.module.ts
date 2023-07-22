b373e03c0f320837998a3e30dc51de363b817394
Doplax <doplax@gmail.com>
2023-07-22 19:22:36 +0200

    feat: Implementado Interceptor de solicitudes HTTP para calcular el timepo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule }from '@angular/common/http';
-import {CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+import { HttpClientModule, HTTP_INTERCEPTORS }from '@angular/common/http';
 import { register } from 'swiper/element/bundle';
-
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ImgComponent } from './components/img/img.component';
 import { ProductComponent } from './components/product/product.component';
 import { ProductsListComponent } from './components/products-list/products-list.component';
-import { ImgComponent } from './components/img/img.component';
 import { NavComponent } from './components/nav/nav.component';
 import { ReversePipe } from './pipes/reverse.pipe';
 import { TimeAgoPipe } from './pipes/time-ago.pipe';
 import { HighlightDirective } from './directives/highlight.directive';
+import { TimeInterceptor } from './interceptors/time.interceptor';
+
+import {CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+
 
 register();
 
@@ -39,7 +41,7 @@ register();
 
 
   ],
-  providers: [],
+  providers: [{provide: HTTP_INTERCEPTORS, useClass:TimeInterceptor, multi: true}],
   bootstrap: [AppComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
